0b7d75b0f4ce916be0bd9812f5f642d0ff39cf61
Natalia <nataliostapiuk@gmail.com>
2021-02-19 01:00:19 +0200

    user registration backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import { authUser} from '../controllers/userController.js'
-
+import { authUser, getUserProfile, registerUser} from '../controllers/userController.js'
+import {protect} from '../midleware/authMiddleware.js'
 
 router.post('/login', authUser)
-
-
+router.route('/profile').get(protect, getUserProfile)
+router.route('/').post(registerUser)
 
 export default router
