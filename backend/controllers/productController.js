16999390155cb66fb64e4a3f6a78d8ac1ea4eda9
Natalia <nataliostapiuk@gmail.com>
2021-02-23 20:50:14 +0200

    update product back

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,6 +3,10 @@ import Product from '../models/productModel.js'
 import asyncHandler from 'express-async-handler'
 
 
+const getProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+    res.send(products)
+})
 
 const getProductById = asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
@@ -41,13 +45,42 @@ const createProduct = asyncHandler(async (req, res) => {
         category: 'Sample category',
         countInStock: 0,
         numReviews: 0,
-        description: 'Sample description',
+        description: 'Description',
     })
+    const createdProduct = await product.save()
+    res.status(201).send(createdProduct)
 })
 
-const getProducts = asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.send(products)
+const updateProduct = asyncHandler(async (req, res) => {
+    const {
+        name,
+        price,
+        description,
+        image,
+        brand,
+        category,
+        countInStock,
+    } = req.body
+
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+        product.name = name
+        product.price = price
+        product.description = description
+        product.image = image
+        product.brand = brand
+        product.category = category
+        product.countInStock = countInStock
+
+        const updatedProduct = await product.save()
+        res.send(updatedProduct)
+    } else {
+        res.status(404)
+        throw new Error('Product not found')
+    }
 })
 
-export { getProductById, getProducts, deleteProduct, createProduct }
+
+
+export { getProductById, getProducts, deleteProduct, createProduct, updateProduct }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,17 @@
 import express from 'express'
 const router = express.Router()
-import { getProductById, getProducts, deleteProduct, createProduct } from '../controllers/productController.js'
+import {
+    getProductById,
+    getProducts,
+    deleteProduct,
+    createProduct,
+    updateProduct
+} from '../controllers/productController.js'
 import { protect, admin } from '../midleware/authMiddleware.js'
 
 router.route('/').get(getProducts)
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
+router.route('/:id').get(getProductById)
+    .delete(protect, admin, deleteProduct)
 
 
 export default router
